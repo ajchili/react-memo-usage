781b09dc5546d88b52efe547246a7d9dcfddc99a
Kirin Patel <kirin.patel@datavant.com>
2024-07-19 16:42:41 -0400

    🗑️ remove tanstack

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -1,9 +1,7 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createRouter, RouterProvider } from "@tanstack/react-router";
 
-import { routeTree } from "./routeTree.gen.js";
 import "./index.css";
+import { App } from "./App.js";
 
 let $app = document.querySelector("div#app");
 
@@ -12,11 +10,5 @@ if (!$app) {
   document.body.appendChild($app);
 }
 
-const router = createRouter({ routeTree });
-
 const root = createRoot($app);
-root.render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-);
+root.render(<App />);
